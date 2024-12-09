57280176dda9062692a8b1e2313b1099d24a51b2
shaker <hydrashaker@gmail.com>
2024-12-09 11:45:45 +0530

    11

---
+++ app.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function add(num1, num2) {
+    return num1 + num2;
+}
+var num1 = "shaker ";
+var num2 = 34;
+console.log(add(num1, num2));
---
+++ app.ts
@@ -0,0 +1,10 @@
+import { log } from "console";
+
+function add(num1 :  number, num2: number){
+    return num1 + num2;
+}
+
+let num1 = 7
+let num2 = 34
+
+console.log(add(num1, num2));
