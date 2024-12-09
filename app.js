cdae62510e5a2d21fef7c3d15397f0a6835c3815
shaker <hydrashaker@gmail.com>
2024-12-09 11:52:51 +0530

    12

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 function add(num1, num2) {
+    if (typeof num1 !== "number" && typeof num2 !== "number") {
+        throw new Error('Incorrect input');
+    }
     return num1 + num2;
 }
-var num1 = "shaker ";
-var num2 = 34;
+var num1 = 7;
+var num2 = '34';
 console.log(add(num1, num2));
+console.log(typeof num2);
--- app.ts
+++ app.ts
@@ -1,6 +1,8 @@
-import { log } from "console";
 
 function add(num1 :  number, num2: number){
+    if(typeof num1 !== "number" && typeof num2 !== "number") {
+        throw new Error('Incorrect input')
+    }
     return num1 + num2;
 }
 
@@ -8,3 +10,5 @@ let num1 = 7
 let num2 = 34
 
 console.log(add(num1, num2));
+console.log(typeof num2);
+
