dab640bf4774d3d648678ea1d365bf5cb9869b05
shaker <hydrashaker@gmail.com>
2024-12-09 13:48:47 +0530

    19

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
-function add(num1, num2) {
-    if (typeof num1 !== "number" && typeof num2 !== "number") {
-        throw new Error('Incorrect input');
+function add(num1, num2, showResult, phrase) {
+    // if(typeof num1 !== "number" && typeof num2 !== "number") {
+    //     throw new Error('Incorrect input')
+    //}
+    var result = num1 + num2;
+    if (showResult) {
+        console.log(phrase + result);
+    }
+    else {
+        return result;
     }
     return num1 + num2;
 }
 var num1 = 7;
-var num2 = '34';
-console.log(add(num1, num2));
-console.log(typeof num2);
+var num2 = 34;
+var printResult = true;
+var resultPhrase = 'Result is ';
+add(num1, num2, printResult, resultPhrase);
+var Person = {
+    name: "shaker",
+    age: 30,
+    hobbies: ['sports', 'Cooking'],
+    role: [1, 'author']
+};
+console.log(Person.name);
+var favoriteActivity;
+favoriteActivity = ["string"];
+console.log(favoriteActivity);
--- app.ts
+++ app.ts
@@ -1,7 +1,13 @@
 
-function add(num1 :  number, num2: number  ){
-    if(typeof num1 !== "number" && typeof num2 !== "number") {
-        throw new Error('Incorrect input')
+function add(num1 :  number, num2: number, showResult : boolean,  phrase: string  ){
+    // if(typeof num1 !== "number" && typeof num2 !== "number") {
+    //     throw new Error('Incorrect input')
+    //}
+    const result = num1 + num2
+    if(showResult){
+        console.log(phrase + result)
+    }else{
+        return result;
     }
     return num1 + num2;
 }
@@ -9,6 +15,31 @@ function add(num1 :  number, num2: number  ){
 let num1 = 7
 let num2 = 34
 let printResult = true
+const resultPhrase = 'Result is '
 
+
+add(num1, num2, printResult, resultPhrase)
+
+
+const Person :{
+    name : string;
+    age : number;
+    hobbies: string[];
+    role: [number, string];
+
+} 
+ = {
+    name : "shaker",
+    age : 30,
+    hobbies: ['sports', 'Cooking'],
+    role: [1, 'author']
+
+}
+
+console.log(Person.name);
+
+let favoriteActivity : string[];
+favoriteActivity = ["string"]
+console.log(favoriteActivity);
 
 
