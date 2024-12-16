d8d642843dab940866eeb71f9dc22671773967e4
shaker <hydrashaker@gmail.com>
2024-12-16 09:47:12 +0530

    .

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+class Department {
+    name: string;
+
+    constructor(n: string){
+        this.name = n;
+    }
+}
+
+const accounting = new Department('accounting')
+
+console.log(accounting);
